cf71fa2b19a4cfba809b24f3e447e50f9e4b9d7b
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-02-09 16:50:20 -0500

    updates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-const db = require("./config/connection");
+const db = require("./server/config/connection");
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./server/schemas");
+const { authMiddleware } = require("./server/utils/auth");
 //const routes = require('./routes');
 
 const app = express();
